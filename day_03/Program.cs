e4557621ec2b7424e8d50fead0d705e5f31ee7b0
qdelamea <qdelamea@aneo.fr>
2024-12-04 20:33:31 +0100

    feat: day 3 part 1

---
+++ day_03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+
+static class Program {
+    static void Main() {
+        const string inputPath = "../data/day_03/input.txt";
+        var sr = File.OpenText(inputPath);
+        var data = sr.ReadToEnd();
+        var pattern = @"mul\((\d+),(\d+)\)";
+        var regex = new Regex(pattern);
+        int result = 0;
+        foreach (Match match in regex.Matches(data)) {
+            result += Int32.Parse(match.Groups[1].Value) * Int32.Parse(match.Groups[2].Value);
+        }
+        Console.WriteLine($"Result: {result}.");
+    }
+}
