322c555821003e44ae8715ca740c24e464f8639b
qdelamea <qdelamea@aneo.fr>
2024-12-06 00:33:42 +0100

    feat: day 4 part 2

--- day_04/Program.cs
+++ day_04/Program.cs
@@ -4,14 +4,16 @@
 static class Program {
     static void Main() {
         string inputPath = "../data/day_04/input.txt";
-        string word = "XMAS";
-        string reversedWord = new string(word.ToCharArray().Reverse().ToArray());
-        int result = ParseInput(inputPath)
-            .IterRawsColumnsAndDiagonals()
-            .Where(s => s.Length >= 4)
-            .Select(s => Regex.Matches(s, word).Count() + Regex.Matches(s, reversedWord).Count())
-            .Sum();
-        
+        var charMatrix = ParseInput(inputPath);
+        int result = 0;
+        for (int i = 1; i < charMatrix.GetLength(0) - 1; i++) {
+            for (int j = 1; j < charMatrix.GetLength(1) - 1; j++) {
+                if (charMatrix[i,j] == 'A' && IsMas(charMatrix, new Tuple<int, int>(i-1,j-1), new Tuple<int, int>(i+1,j+1)) && IsMas(charMatrix, new Tuple<int, int>(i-1,j+1), new Tuple<int, int>(i+1,j-1))) {
+                    result++;
+                }
+            }
+        }
+
         Console.WriteLine($"Result: {result}.");
     }
 
@@ -26,61 +28,11 @@ static class Program {
         }
         return charMatrix;
     }
-}
 
-
-public static class CharMatrixExtensions {
-    public static IEnumerable<string> IterRawsColumnsAndDiagonals(this char[,] charMatrix) {
-        int n = charMatrix.GetLength(0);
-        for (int i = 0; i < n; i++) {
-            yield return charMatrix.GetRaw(i);
-            yield return charMatrix.GetCol(i);
-            yield return charMatrix.GetDiag(i);
-            yield return charMatrix.GetDiag(i, true);
-            if (i > 0) {
-                yield return charMatrix.GetDiag(-1 * i);
-                yield return charMatrix.GetDiag(-1 * i, true);
-            }
+    public static bool IsMas(char[,] charMatrix, Tuple<int,int> firstIndices, Tuple<int,int> secondIndices) {
+        if ((charMatrix[firstIndices.Item1, firstIndices.Item2] == 'M' && charMatrix[secondIndices.Item1, secondIndices.Item2] == 'S') || (charMatrix[firstIndices.Item1, firstIndices.Item2] == 'S' && charMatrix[secondIndices.Item1, secondIndices.Item2] == 'M')) {
+            return true;
         }
-    }
-
-    public static string GetRaw(this char[,] charMatrix, int rawIndex) {
-        int n = charMatrix.GetLength(0);
-        char[] raw = new char[n];
-        for (int j = 0; j < n; j++) {
-            raw[j] = charMatrix[rawIndex, j];
-        }
-        return new string(raw);
-    }
-
-    public static string GetCol(this char[,] charMatrix, int colIndex) {
-        int n = charMatrix.GetLength(0);
-        char[] col = new char[n];
-        for (int j = 0; j < n; j++) {
-            col[j] = charMatrix[j, colIndex];
-        }
-        return new string(col);
-    }
-
-    public static string GetDiag(this char[,] charMatrix, int diagIndex, bool transpose = false) {
-        int n = charMatrix.GetLength(0);
-        int m = Math.Abs(diagIndex);
-        char[] diag = new char[n - m];
-        for (int j = 0; j < n - m; j++) {
-            if (diagIndex >= 0) {
-                if (transpose) {
-                    diag[j] = charMatrix[j + diagIndex, n - 1 - j];
-                } else {
-                    diag[j] = charMatrix[j, j + diagIndex];
-                }
-            } else {
-                if (transpose) {
-                    diag[j] = charMatrix[j, n - 1 - j + diagIndex];
-                } else {
-                    diag[j] = charMatrix[j - diagIndex, j];
-                }
-            }
-        }
-        return new string(diag);
+        return false;
     }
 }
