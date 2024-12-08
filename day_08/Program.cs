fd383092abd7c9b968e3df7fa3efb6ef31f89764
qdelamea <qdelamea@aneo.fr>
2024-12-08 15:12:47 +0100

    feat: day 8 part 2

--- day_08/Program.cs
+++ day_08/Program.cs
@@ -7,15 +7,9 @@
             .Where(kvp => kvp.Value.Count() > 1)
             .Select(kvp => kvp.Value
                     .SelectMany((_, i) => kvp.Value.Skip(i + 1), (x, y) => (x, y))
-                    .Select(pair => {
-                        var (x1, x2) = pair.x;
-                        var (y1, y2) = pair.y;
-                        var delta = (x1 - y1, x2 - y2);
-                        return new List<(int, int)> {(x1 + delta.Item1, x2 + delta.Item2), (y1 - delta.Item1, y2 - delta.Item2)};
-                    })
+                    .Select(pair => ListAntinodes(pair.x, pair.y, (pair.x.Item1 - pair.y.Item1, pair.x.Item2 - pair.y.Item2), dimensions))
                     .SelectMany(list => list)
             ).SelectMany(list => list)
-            .Where(coord => 0 <= coord.Item1 && coord.Item1 < dimensions.Item1 && 0 <= coord.Item2 && coord.Item2 < dimensions.Item2)
             .Distinct()
             .Count();
 
@@ -40,5 +34,20 @@
             }
         }
         return (antennas, (N, M));
+    }
+
+    public static List<(int, int)> ListAntinodes((int, int) anthenna1, (int, int) anthenna2, (int, int) delta, (int, int) dimensions) {
+        var antinodes = new List<(int, int)>();
+        var currentPosition = anthenna1;
+        while (0 <= currentPosition.Item1 && currentPosition.Item1 < dimensions.Item1 && 0 <= currentPosition.Item2 && currentPosition.Item2 < dimensions.Item2) {
+            antinodes.Add(currentPosition);
+            currentPosition = (currentPosition.Item1 + delta.Item1, currentPosition.Item2 + delta.Item2);
+        }
+        currentPosition = anthenna2;
+        while (0 <= currentPosition.Item1 && currentPosition.Item1 < dimensions.Item1 && 0 <= currentPosition.Item2 && currentPosition.Item2 < dimensions.Item2) {
+            antinodes.Add(currentPosition);
+            currentPosition = (currentPosition.Item1 - delta.Item1, currentPosition.Item2 - delta.Item2);
+        }
+        return antinodes;
     }
 }
