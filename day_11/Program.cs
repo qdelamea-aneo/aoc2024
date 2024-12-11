88880b2018d959e2844f8a7bfe1a8774a98e9e16
qdelamea <qdelamea@aneo.fr>
2024-12-11 10:31:35 +0100

    feat: day 11 part 1

---
+++ day_11/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program {
+    static void Main() {
+        string inputPath = "../data/day_11/input.txt";
+        var stones = File
+            .ReadAllText(inputPath)
+            .Split(" ")
+            .Select(s => Int64.Parse(s));
+        
+        int numBlinks = 25;
+
+        for (int i = 0; i < numBlinks; i++) {
+            stones = stones
+                .SelectMany(n => {
+                    if (n == 0) {
+                        return new List<long> { 1 };
+                    } else if (n.ToString().Length % 2 == 0) {
+                        var str = n.ToString();
+                        var cutIndex = str.Length / 2;
+                        return new List<long> { Int64.Parse(str.Substring(0, cutIndex)), Int64.Parse(str.Substring(cutIndex)) };
+                    } else {
+                        return new List<long> { n * 2024 };
+                    }
+                });
+        }
+
+        Console.WriteLine($"Result: {stones.Count()}.");
+    }
+}
