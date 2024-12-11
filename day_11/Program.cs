20ce6a3888b6597c02e46d7b434d14da89b4697c
qdelamea <qdelamea@aneo.fr>
2024-12-11 11:50:31 +0100

    feat: day 11 part 2

--- day_11/Program.cs
+++ day_11/Program.cs
@@ -6,23 +6,43 @@
             .Split(" ")
             .Select(s => Int64.Parse(s));
         
-        int numBlinks = 25;
+        long numBlinks = 75;
+        long result = 0;
+        var memory = new Dictionary<(long, long), long>();
 
-        for (int i = 0; i < numBlinks; i++) {
-            stones = stones
-                .SelectMany(n => {
-                    if (n == 0) {
-                        return new List<long> { 1 };
-                    } else if (n.ToString().Length % 2 == 0) {
-                        var str = n.ToString();
-                        var cutIndex = str.Length / 2;
-                        return new List<long> { Int64.Parse(str.Substring(0, cutIndex)), Int64.Parse(str.Substring(cutIndex)) };
-                    } else {
-                        return new List<long> { n * 2024 };
-                    }
-                });
+        foreach (var stone in stones) {
+            result += GetNumberOfStones(stone, numBlinks, memory);
         }
 
-        Console.WriteLine($"Result: {stones.Count()}.");
+        Console.WriteLine($"Result: {result}.");
+    }
+
+    static long GetNumberOfStones(long stone, long numBlinks, Dictionary<(long, long), long> memory) {
+        if (memory.ContainsKey((stone, numBlinks))) {
+            return memory[(stone, numBlinks)];
+        }
+        if (numBlinks == 0) {
+            memory[(stone, numBlinks)] = 1;
+            return 1;
+        }
+        if (stone == 0) {
+            var num = GetNumberOfStones(1, numBlinks - 1, memory);
+            memory[(stone, numBlinks)] = num;
+            return num;
+        } else if (stone.ToString().Length % 2 == 0) {
+            var str = stone.ToString();
+            var cutIndex = str.Length / 2;
+            var stone1 = Int64.Parse(str.Substring(0, cutIndex));
+            var stone2 = Int64.Parse(str.Substring(cutIndex));
+
+            var num1 = GetNumberOfStones(stone1, numBlinks - 1, memory);
+            var num2 = GetNumberOfStones(stone2, numBlinks - 1, memory);
+            memory[(stone, numBlinks)] = num1 + num2;
+            return num1 + num2;
+        } else {
+            var num = GetNumberOfStones(stone * 2024, numBlinks - 1, memory);
+            memory[(stone, numBlinks)] = num;
+            return num;
+        }
     }
 }
