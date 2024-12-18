b586da226127f4ee7c6a9d77d3772f1d2146f02a
qdelamea <qdelamea@aneo.fr>
2024-12-18 21:36:19 +0100

    feat: day 16 part 2

--- day_16/part_1.py
+++ day_16/part_1.py
@@ -16,6 +16,9 @@ class Node:
     def __gt__(self, other: "Node") -> bool:
         return True
 
+    def __eq__(self, other: "Node") -> bool:
+        return self.position == other.position and self.direction == other.direction
+
 
 class Graph:
 
@@ -42,6 +45,8 @@ class Graph:
         return Node(self.start, (0, 1))
 
     def neighbors(self, v: Node) -> list[Node]:
+        if v.position == self.end:
+            return []
         directions = {(1, 0), (-1, 0), (0, 1), (0, -1)} - {(-v.direction[0], -v.direction[1])}
         neighbors = set()
         for direction in directions:
@@ -62,8 +67,19 @@ class Graph:
 
 
 def main(input_path: Path) -> int:
+    def build_paths(node: Node) -> list[Node]:
+        if node == graph.source_vertex:
+            return [[node]]
+        
+        paths = []
+        for pred in preds[node]:
+            for path in build_paths(pred):
+                paths.append(path + [node])
+        return paths
+
     graph = Graph.load_from_file(input_path)
     dist = defaultdict(lambda: float("inf"))
+    preds = defaultdict(list)
     visited = defaultdict(bool)
     priority_queue = []
 
@@ -83,9 +99,14 @@ def main(input_path: Path) -> int:
                 alt = dist[u] + graph.edge_weight(u, neighbor)
                 if alt < dist[neighbor]:
                     dist[neighbor] = alt
+                    preds[neighbor] = [u]
                     heapq.heappush(priority_queue, (dist[neighbor], neighbor))
+                elif alt == dist[neighbor]:
+                    preds[neighbor].append(u)
 
-    return min([score for v, score in dist.items() if v.position == graph.end])
+    best_score = min([score for v, score in dist.items() if v.position == graph.end])
+    shortest_paths = [path for final_node, score in dist.items() if final_node.position == graph.end and score == best_score for path in build_paths(final_node)]
+    return len(set([node.position for shortest_path in shortest_paths for node in shortest_path]))
 
 
 if __name__ == "__main__":
